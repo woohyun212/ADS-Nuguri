4c149d816b831834715620007318fa003c167f91
woohyun <43837268+woohyun212@users.noreply.github.com>
2025-11-26 11:05:58 +0900

    [feat] Add beep function to play sound on coin collection

--- nuguri.c
+++ nuguri.c
@@ -74,6 +74,7 @@ void ending(void);
 void cls_screen(void);
 void void_screen();
 void cls_mem();
+void beep();
 
 int main()
 {
@@ -608,6 +609,7 @@ void check_collisions()
         {
             coins[i].collected = 1;
             score += 20;
+            beep();
         }
     }
 }
@@ -892,4 +894,8 @@ void cls_screen(void)
 {
     // 커서만 0,0 으로 이동
     printf("\x1b[H");
+}
+
+void beep() {
+    printf("\a"); // 비프음 발생
 }
